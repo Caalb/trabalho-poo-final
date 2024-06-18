46506e5d022ed1fd7e16a72de52f941d83bfd18e
Caalb <carlosmunizfjunior@gmail.com>
2024-06-18 20:02:02 -0300

    fix(Program.cs): corrige erro IReceiptService

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IAccountService, AccountService>();
+builder.Services.AddTransient<IReceiptService, ReceiptService>();
 
 var app = builder.Build();
 
